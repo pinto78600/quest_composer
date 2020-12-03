8cc1154698ad184f8394678552e6bc2153bdea86
pinto78600 <pintostephane92@hotmail.fr>
2020-12-03 17:12:17 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+require  dirname(__DIR__). '/vendor/autoload.php';
+use App\Wcs\Hello;
+
+$obj = new Hello;
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Wcs;
+
+class Hello 
+{
+    public function __construct()
+    {
+      echo 'Hello World';
+    }
+}
